5c81c0f233ff55e743a0f3b334ce7779f3cae780
David-Adeleke <damoshey08@gmail.com>
2026-02-12 03:43:36 +0100

    feat: added smart navbar change on authentication

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,7 +1,15 @@
 import { Menubar, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { isAuthenticated, logout } from '../utils/auth';
 
 function Navigation() {
+  const navigate = useNavigate()
+  const authenticated = isAuthenticated()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/login')
+  }
   return (
     <nav className="flex justify-between gap-6 mx-auto items-center">
       <h1 className="text-xl font-bold">MONKE</h1>
@@ -12,26 +20,38 @@ function Navigation() {
           </MenubarTrigger>
         </MenubarMenu>
 
-        <MenubarMenu>
-          <MenubarTrigger>
-            <Link to="/todo">Todos</Link>
-          </MenubarTrigger>
-        </MenubarMenu>
+        {!authenticated ? (
+          <>
+            <MenubarMenu>
+              <MenubarTrigger asChild>
+                <Link to="/login">Login</Link>
+              </MenubarTrigger>
+            </MenubarMenu>
 
-        <MenubarMenu>
-          <MenubarTrigger>
-            <Link to="/login">Login</Link>
-          </MenubarTrigger>
-        </MenubarMenu>
-
-        <MenubarMenu>
-          <MenubarTrigger>
-            <Link to="register">Register</Link>
-          </MenubarTrigger>
-        </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger asChild>
+                <Link to="/register">Register</Link>
+              </MenubarTrigger>
+            </MenubarMenu>
+          </>
+        ) : (
+          <>
+            <MenubarMenu>
+              <MenubarTrigger>
+                <Link to="/todo">Todos</Link>
+              </MenubarTrigger>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger onClick={handleLogout}>
+                Logout
+              </MenubarTrigger>
+            </MenubarMenu>
+          </>
+        )}
       </Menubar>
     </nav>
   );
 }
+
 
 export default Navigation;
---
+++ src/utils/auth.js
@@ -0,0 +1,8 @@
+export const isAuthenticated = () => {
+    return !!localStorage.getItem("accessToken")
+}
+
+export const logout = () => {
+    localStorage.removeItem("accessToken")
+    localStorage.removeItem("refreshToken")
+}
